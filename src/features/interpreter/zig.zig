2604ebcdf0079008e88b70753525364d12f44f40
LeviathanST <phamquochung170505@gmail.com>
2025-11-23 12:22:00 +0700

    interpreter/zig: line iteration

--- src/features/interpreter/zig.zig
+++ src/features/interpreter/zig.zig
@@ -6,7 +6,10 @@ const Command = Interpreter.Command;
 
 const Ast = std.zig.Ast;
 
-const Main = struct {
+// Contains all called functions in the main
+const MainNode = std.ArrayList(Node);
+
+const Node = struct {
     fn_name: []const u8,
     arg_value: []const u8,
     arg_node_tag: Ast.Node.Tag,
@@ -16,24 +19,30 @@ pub fn parse(
     alloc: std.mem.Allocator,
     interpreter: *Interpreter,
     source: [:0]const u8,
-) !Command {
+) ![]Command {
+    var commands: std.ArrayList(Command) = .empty;
     const command_parser: Command.Parser = .init(alloc, interpreter);
     const ast = try Ast.parse(alloc, source, .zig);
 
     try extractErrorFromAst(alloc, interpreter, ast);
-    if (interpreter.errors.items.len > 0) return .none;
+    if (interpreter.errors.items.len > 0) return &.{};
 
     const main = (try parseMainNode(
         alloc,
         interpreter,
         ast,
-    )) orelse return .none;
+    )) orelse return &.{};
 
-    return command_parser.parse(
-        main.fn_name,
-        main.arg_value,
-        main.arg_node_tag,
-    );
+    for (main.items) |node| {
+        const command = try command_parser.parse(
+            node.fn_name,
+            node.arg_value,
+            node.arg_node_tag,
+        );
+
+        try commands.append(alloc, command);
+    }
+    return commands.toOwnedSlice(alloc);
 }
 
 /// Get the first `main` function node index from AST
@@ -62,24 +71,26 @@ pub fn isMain(ast: Ast, index: Ast.Node.Index) bool {
 
     if (fn_proto.name_token) |token_fn_name_idx| {
         const fn_name = ast.tokenSlice(token_fn_name_idx);
-        std.log.debug("{s}", .{fn_name});
         if (std.mem.eql(u8, "main", fn_name)) return true;
     }
     return false;
 }
 
+/// Return `null` if the main node not found.
+/// This function can cause to panic due to out of memory.
 fn parseMainNode(
     alloc: std.mem.Allocator,
     interpreter: *Interpreter,
     ast: Ast,
-) !?Main {
+) !?MainNode {
+    var nodes: MainNode = .empty;
     // TODO: enable user to declare custom functions, variables, ..., like normal.
     // NOTE: currently, players can only declare the `main` function
     //       and use available functions ingame.
 
     // get main node (`fn main()`)
     const main_node_idx = getMainNodeIdx(ast) orelse {
-        interpreter.appendError(alloc, .{
+        try interpreter.appendError(alloc, .{
             .tag = .main_not_found,
             .token = "",
         });
@@ -91,30 +102,29 @@ fn parseMainNode(
     var call_node_buf: [2]Ast.Node.Index = undefined;
     const call_node_idxs = ast.blockStatements(&call_node_buf, block_node_idx).?;
 
-    // NOTE: player can only do a action at a time
-    //
-    // get the first `call` node
-    const idx = call_node_idxs[0];
-    var call_buf: [1]Ast.Node.Index = undefined;
-    const call = ast.fullCall(&call_buf, idx).?;
+    for (call_node_idxs) |idx| {
+        var call_buf: [1]Ast.Node.Index = undefined;
+        const call = ast.fullCall(&call_buf, idx).?;
 
-    // Extract the call node
-    const fn_name_tok_i = ast.nodes.get(@intFromEnum(call.ast.fn_expr)).main_token;
-    const fn_name = ast.tokenSlice(fn_name_tok_i);
+        // Extract the call node
+        const fn_name_tok_i = ast.nodes.get(@intFromEnum(call.ast.fn_expr)).main_token;
+        const fn_name = ast.tokenSlice(fn_name_tok_i);
 
-    // NOTE: we know that just one param in the function now.
-    //       EX: `move(.up)`, `move(.down)`
-    const arg_idx = call.ast.params[0];
-    const arg_node_tag = ast.nodeTag(arg_idx);
+        // NOTE: we know that just one param in the function now.
+        //       EX: `move(.up)`, `move(.down)`
+        const arg_idx = call.ast.params[0];
+        const arg_node_tag = ast.nodeTag(arg_idx);
 
-    const arg_tok_i = ast.nodes.get(@intFromEnum(arg_idx)).main_token;
-    const arg_value = ast.tokenSlice(arg_tok_i);
+        const arg_tok_i = ast.nodes.get(@intFromEnum(arg_idx)).main_token;
+        const arg_value = ast.tokenSlice(arg_tok_i);
 
-    return .{
-        .fn_name = fn_name,
-        .arg_value = arg_value,
-        .arg_node_tag = arg_node_tag,
-    };
+        try nodes.append(alloc, .{
+            .fn_name = fn_name,
+            .arg_value = arg_value,
+            .arg_node_tag = arg_node_tag,
+        });
+    }
+    return nodes;
 }
 
 fn extractErrorFromAst(
@@ -131,7 +141,7 @@ fn extractErrorFromAst(
             try list.append(alloc, try allocating_writer.toOwnedSlice());
         }
 
-        interpreter.appendError(alloc, .{
+        try interpreter.appendError(alloc, .{
             .tag = .from_languages,
             .extra = .{
                 .from_languages = try list.toOwnedSlice(alloc),
